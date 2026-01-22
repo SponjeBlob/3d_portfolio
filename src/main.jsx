53b7c745d5e49ff3c4f714f3bc312ba5d4644e0d
SponjeBlob <hugomazariegosmartin@gmail.com>
2026-01-22 05:04:23 +0000

    something

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+// Import favicon so Vite copies it to the build
+import './assets/bobak.ico';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
