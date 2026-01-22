13b00f2c84a742abc38972932a22df7caff8151f
SponjeBlob <hugomazariegosmartin@gmail.com>
2026-01-22 05:57:28 +0000

    the working version

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// ...
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { Home, About, Projects, Contact } from './pages';
+import Navbar from './Components/Navbar';
 
 const App = () => {
   return (
     <main className="bg-slate-300/2 min-h-screen">
-      <Router basename="/3d_portfolio">
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects" element={<Projects />} /> {/* Landing page */}
+
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </Router>
+      </Router> 
     </main>
-  );
-};
+  )
+}
 
-export default App;
+export default App
